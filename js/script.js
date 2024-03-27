afa1cc1f487d79e567874574cf9cd328a663f2c0
Shuvo458 <155560696+Shuvo458@users.noreply.github.com>
2024-03-27 20:46:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//jq
+$(document).ready(function(){
+      // hide
+      $('#hide').on('click', function(){
+        $('#tsh').hide();
+    });
+    $('#show').on('click', function(){
+        $('#tsh').show();
+    });
+})
